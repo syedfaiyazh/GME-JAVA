70c954fdab8c92060b04f7eb708b1cded20dc93a
Syed Hussain <you@example.com>
2020-05-30 18:25:14 -0400

    Initial push

---
+++ Vehicle.java
@@ -0,0 +1,18 @@
+abstract class vehicle
+{
+   abstract void start();
+}
+class car extends vehicle
+{
+void start()
+ {
+System.out.println("key");
+ } 
+}
+class scooter extends vehicle
+{
+void start()
+ {
+System.out.println("kick");
+ }
+}
